92a89bf06c2e711ad86344c27333e3b379083842
Anthony Kewin <kewintk4@gmail.com>
2025-05-15 15:45:48 +0000

    new lecture

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+// defining app as express and port
+const app = express();
+const port = 3000;
+
+app.port(';')
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+})
