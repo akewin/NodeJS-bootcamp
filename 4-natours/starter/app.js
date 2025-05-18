10f092a86a0831b8f84596411565ce6a5f0e4b3e
Anthony Kewin <kewintk4@gmail.com>
2025-05-18 12:32:28 +0000

    feature: incrementing app.js

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 // defining app as express and port
 const app = express();
-const port = 3000;
+const port = 3333;
 
-app.port(';')
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'hello from the server side', app: 'Natorus'});
+});
+
+app.post('/', (req, res) => {
+    res.status(200).send("you can post to this endpoint.");
+})
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
