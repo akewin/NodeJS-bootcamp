74bfb999eca6ffa41bc438da23a61f91cf93caca
Anthony Kewin <kewintk4@gmail.com>
2025-05-23 22:44:42 +0000

    natours implementation

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,16 +1,43 @@
 const express = require("express");
+const fs = require("node:fs");
 // defining app as express and port
 const app = express();
-const port = 3333;
+const port = 3334;
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'hello from the server side', app: 'Natorus'});
+// middleware to post in json
+app.use(express.json());
+
+//reading tour data
+const tours = JSON.parse(
+	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+
+// enveloping json from file and sending back
+app.get("/api/v1/tours", (req, res) => {
+	res.status(200).json({
+		status: "success",
+		results: tours.length,
+		data: { tours },
+	});
 });
 
-app.post('/', (req, res) => {
-    res.status(200).send("you can post to this endpoint.");
-})
+// the post route
+app.post("/api/v1/tours", (req, res) => {
+	// console.log(req.body);
+
+	const newID = tours[tours.length - 1].id + 1;
+	const newTour = Object.assign({ id: newID }, req.body);
+
+	tours.push(newTour);
+	fs.writeFile(
+		`${__dirname}/dev-data/data/tours-simple.json`,
+		JSON.stringify(tours),
+		(err) => {},
+	);
+
+	res.send("done.");
+});
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-})
+	console.log(`App running on port ${port}`);
+});
